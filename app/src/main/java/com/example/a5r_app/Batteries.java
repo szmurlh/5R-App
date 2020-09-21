92db65cb7f85fe87cfc1ce76e64a6211310788d1
Hannah Szmurlo <szmurlh@sunypoly.edu>
2020-09-21 18:09:48 -0400

    Initial commit 9/21/2020

---
+++ app/src/main/java/com/example/a5r_app/Batteries.java
@@ -0,0 +1,14 @@
+package com.example.a5r_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Batteries extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_batteries);
+    }
+}
