7489e128b6eaaa0d7251d1eed88ebc8d0f643a04
Max Shaffer <max.r.shaffer@gmail.com>
2020-09-30 16:39:13 -0400

    testing get and post

--- app/src/main/java/com/example/a5r_app/MainActivity.java
+++ app/src/main/java/com/example/a5r_app/MainActivity.java
@@ -1,6 +1,25 @@
 package com.example.a5r_app;
 
+//max imports
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -14,6 +33,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView get_response_text,post_response_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +51,28 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button get_request_button=findViewById(R.id.get_data);
+        //Button post_request_button=findViewById(R.id.post_data);
+
+        get_response_text=findViewById(R.id.get_respone_data);
+        //post_response_text=findViewById(R.id.post_respone_data);
+
+
+        get_request_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendGetRequest();
+            }
+        });
+
+        /*
+        post_request_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                postRequest();
+            }
+        }); */
     }
 
     @Override
@@ -52,5 +95,84 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+/*
+    private void postRequest() {
+        RequestQueue requestQueue=Volley.newRequestQueue(MainActivity.this);
+        String url="urlhere";
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //let's parse json data
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    post_response_text.setText("Data 1 : " + jsonObject.getString("data_1_post")+"\n");
+                    post_response_text.append("Data 2 : " + jsonObject.getString("data_2_post")+"\n");
+                    post_response_text.append("Data 3 : " + jsonObject.getString("data_3_post")+"\n");
+                    post_response_text.append("Data 4 : " + jsonObject.getString("data_4_post")+"\n");
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                    post_response_text.setText("POST DATA : unable to Parse Json");
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                post_response_text.setText("Post Data : Response Failed");
+            }
+        }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params=new HashMap<String, String>();
+                params.put("data_1_post","Value 1 Data");
+                params.put("data_2_post","Value 2 Data");
+                params.put("data_3_post","Value 3 Data");
+                params.put("data_4_post","Value 4 Data");
+                return params;
+            }
+
+            @Override
+            public Map<String,String> getHeaders() throws AuthFailureError{
+                Map<String,String> params=new HashMap<String, String>();
+                params.put("Content-Type","application/x-www-form-urlencoded");
+                return params;
+            }
+        };
+
+        requestQueue.add(stringRequest);
+
+    }
+*/
+    private void sendGetRequest() {
+        //get working now
+        //let's try post and send some data to server
+        RequestQueue queue= Volley.newRequestQueue(MainActivity.this);
+        String url="https://jsonblob.com/api/5d1815da-02a9-11eb-9f82-2f0342b0cfd0";
+        StringRequest stringRequest=new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                get_response_text.setText("Data : "+response);
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    get_response_text.setText("Data 1 :"+jsonObject.getString("data_1")+"\n");
+                    get_response_text.append("Data 2 :"+jsonObject.getString("data_2")+"\n");
+                    get_response_text.append("Data 3 :"+jsonObject.getString("data_3")+"\n");
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                    get_response_text.setText("Failed to Parse Json");
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                get_response_text.setText("Data : Response Failed");
+            }
+        });
+
+        queue.add(stringRequest);
     }
 }
